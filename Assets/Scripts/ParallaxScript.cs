16c377cc3e2be70e09e66d575a96816ca84d7066
saltire sable <saltiresable@gmail.com>
2018-11-28 03:32:25 -0500

    Put timeline animation on camera; very rough walk animation

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParallaxScript : MonoBehaviour {
+	public float amount;
+
+	Camera cam;
+
+	void Awake() {
+		cam = GameObject.FindObjectOfType<Camera>();
+	}
+
+	void Update() {
+		foreach (Transform child in transform) {
+			child.position = new Vector3(
+				cam.transform.position.x * child.position.z * amount,
+				cam.transform.position.y * child.position.z * amount,
+				child.position.z);
+		}
+	}
+}
